711c1ec1dea3612a0700218a8edb814c96caf9ec
Suppanut620 <6206021610056@fitm.kmutnb.ac.th>
2019-06-20 00:06:45 +0700

    Create pj1.cpp

---
+++ pj1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Student Code : 6206021622119 " << endl;
+	cout<<"Student Name : Mr.Pawarut Narksuth  "<< endl;
+	cout<<"Division     : Information Technology"<< endl;
+	cout<<"Year         : 1\n";
+	cout<<"Room         : RC\n";
+	cout<<"Sex          : Male\n";
+	cout<<"Age          : 18\n";
+	cout<<"============================" << endl;
+
+	cout<<"               #   \n";
+	cout<<"              # # \n";
+	cout<<"             #   # \n";
+	cout<<"            #     # \n";
+	cout<<"            #######\n";
+	cout<<"            #     #"<<endl;
+	return(0);
+}
