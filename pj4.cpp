aace82c5d461659e385e874c9119d18dd97e1bb3
bestpawarut01 <52008102+bestpawarut01@users.noreply.github.com>
2019-07-09 21:29:05 +0700

    Create pj4.cpp

---
+++ pj4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	int num;
+	  cout << "Enter your score :";
+	  cin >> num;
+	if (num >= 90){
+		cout << " A " << endl;
+	}
+	else if (num <= 89 && num >= 80){
+		cout << " B " << endl;
+	}
+	else if (num <= 79 && num >= 70 ){
+		cout << " C " << endl;
+	}
+	else if (num <= 69 && num >= 60 ){
+		cout << " D " <<endl;
+	}
+	else if (num <= 59){
+		cout << " F " <<endl;
+	}
+	return 0;
+
+}
