e3566f46947d927d2f9ed54f3fd71e29660f85b2
bestpawarut01 <52008102+bestpawarut01@users.noreply.github.com>
2019-08-20 22:11:35 +0700

    Create pj.6.cpp

---
+++ pj.6.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+int checkNum(int);
+int r, num1,num2 = 0;
+int main()
+{	
+	srand(time(NULL));
+	cout << "#### Welcome to gussing number game ####"<<endl;
+	cout << "Secret number has been chosen" << endl;
+	do {
+		r = rand() % 10 + 1;
+		cout << "Guess the number(1 to 10) : ";
+		cin >> num1;
+		num2++;
+		checkNum(num1);
+	} while (num1 != r);
+	cout << "The secret number is " << r << endl;
+	cout << "You made " << num2 << " guesses " << endl;
+	cout << r;
+	return(num1);
+	return 0;
+}
+int checkNum(int num1)
+{
+	if (num1 < r)
+	{
+		cout << "The secret number is lower" << endl;
+	}
+	else if (num1 > r)
+	{
+		cout << "The secret number is higher" << endl;
+	}
+	else if(num1 == r)
+	{
+		cout << " Congratulations! " << endl;
+	}
+	return(num1);
+}
+
